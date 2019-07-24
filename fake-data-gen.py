649994e7674171358dcd60adf78cb1599f0487ef
Javier Sánchez <jsg_sanchez@hotmail.com>
2019-07-24 13:06:15 +1000

    ADD: Command line input for sql Connection to Mysql working fine

--- fake-data-gen.py
+++ fake-data-gen.py
@@ -2,6 +2,7 @@
 import time
 import argparse
 from kafka.producer import Producer
+from sql.sql_conn import SQLConnector
 from generator.data_generator import DataGenerator
 
 
@@ -36,6 +37,25 @@ def main():
     cl_group.add_argument('-b', '--brokers', required=False, dest='kafka_brokers', action='store',
                           help='List of Kafka brokers')
 
+    # SQL commands
+    sql_parser = subparsers.add_parser('sql', help='Write to SQL database')
+    sql_parser.add_argument('-d', '--driver', required=False, dest='sql_driver', action='store',
+                            help='SQL Driver ("mysql" or "postgres")')
+    sql_parser.add_argument('-sh', '--host', required=True, dest='sql_host', action='store',
+                            help='DB Host IP.')
+    sql_parser.add_argument('-p', '--port', dest='sql_port', type=int, action='store',
+                            help='MySQL or PostgreSQL port. Needed only if different than default ones.')
+    sql_parser.add_argument('-db', '--database', required=True, dest='sql_database', action='store',
+                            help='MySQL or PostgreSQL database.')
+    sql_parser.add_argument('-u', '--username', required=True, dest='sql_username', action='store',
+                            help='MySQL or PostgreSQL username.')
+    sql_parser.add_argument('-pw', '--password', required=True, dest='sql_password', action='store',
+                            help='MySQL or PostgreSQL password.')
+    sql_parser.add_argument('-n', '--number-lines', dest='num_lines', type=int, default=10, action='store',
+                            help='Number of lines to generate (default: 10)')
+    sql_parser.add_argument('-s', '--sleep-loop', dest='seconds', type=float, default=0.0, action='store',
+                            help='Write every SECONDS seconds. If SECONDS>0 infinite loop (default:0.0)')
+
     print(parser.parse_args())
     args = parser.parse_args()
     if args.seconds > 0:
@@ -61,7 +81,7 @@ def main():
                 time.sleep(args.seconds)
 
     # Generate data to send to Kafka
-    else:
+    elif 'kafka_topic' in args:
         topic = args.kafka_topic
         properties = args.kafka_props if 'kafka_props' in args else None
         brokers = args.kafka_brokers if 'kafka_brokers' in args else None
@@ -82,6 +102,26 @@ def main():
             for i in range(args.num_lines):
                 producer.send(topic=topic, value=gen.generate(), flush=False)
             producer.flush(True)
+            if infinite:
+                time.sleep(args.seconds)
+    else:
+        driver = args.sql_driver
+        host = args.sql_host
+        database = args.sql_database
+        username = args.sql_username
+        password = args.sql_password
+        port = args.sql_port if 'sql_port' in args else None
+
+        sql_conn = SQLConnector(driver=driver, host=host, port=port, database=database, username=username,
+                                password=password)
+
+        from generator.templates.credit_card_template import CreditCardTemplate
+        gen = DataGenerator(CreditCardTemplate(delimiter=","))
+
+        while infinite:
+            for i in range(args.num_lines):
+                query = 'INSERT ...'
+                sql_conn.query_exec(query)
             if infinite:
                 time.sleep(args.seconds)
 
